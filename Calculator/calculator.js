3e93bd659c5d4e76c496896e76d133f836915355
Zariyah <100388041+ZariyahDevLab@users.noreply.github.com>
2023-05-18 20:45:55 -0400

    Add files via upload
    CSS has been applied and small fixes have been made. The calculator is fully functional but the input field disappears when a number is clicked.

--- Calculator/calculator.js
+++ Calculator/calculator.js
@@ -1,17 +1,137 @@
-var result = document.getElementById("result");
+//Get refrences to HTML elements
+const display = document.getElementById('display');
 
-function addToResult(value) {
-    result.value += value;
-}
+const buttons = document.querySelectorAll('.btn');
 
-function clearResult() {
-    result.value = "";
-}
 
-function calculate() {
-    try {
-        result.value = eval(result.value);
-    } catch (err) {
-        alert("Invalid Calculation");
+//Initialize Variables
+let firstOperand = '';
+
+let operator = '';
+
+let secondOperand = '';
+
+
+//Add event listeners to buttons
+buttons.forEach(button => {
+
+    button.addEventListener('click', handleButtonClick);
+
+});
+
+
+//Event listener for button clicks
+function handleButtonClick(e) {
+    const value = e.target.innerText;
+
+    //Check if the clicked button is a number or operator
+    if(!isNaN(value) || value === '.') {
+
+        handleNumberClick(value);
+
+    } else {
+
+        handleOperatorClick(value);
+
     }
 }
+
+//Handle number button clicks
+function handleNumberClick(value) {
+
+    if (operator === '') {
+
+        firstOperand += value;
+
+    } else {
+
+        secondOperand += value;
+
+    }
+
+    updateDisplay();
+
+}
+
+//Handle operator button clicks
+function handleOperatorClick(value) {
+
+    if(value === '=') {
+
+        calculate();
+
+    } else if (value === 'C') {
+
+        clear();
+
+        updateDisplay();
+
+    } else {
+
+        operator = value;
+
+    }
+}
+
+//Perform the calculation
+function calculate() {
+
+    const num1 = parseFloat(firstOperand);
+
+    const num2 = parseFloat(secondOperand);
+
+
+    let result;
+
+    switch (operator) {
+
+        case '+':
+            result = num1 + num2;
+            break;
+
+        case '-':
+            result = num1 - num2;
+            break;
+
+        case '*':
+            result = num1 * num2;
+            break;
+
+        case '/':
+            result = num1 / num2;
+            break;
+    }
+
+    //Reset variables
+    firstOperand = result.toString();
+
+    operator = '';
+
+    secondOperand = '';
+
+
+    updateDisplay();
+}
+
+//Clear the calculator
+function clear() {
+
+    firstOperand = '';
+
+    operator = '';
+
+    secondOperand = '';
+
+
+    updateDisplay();
+
+}
+
+//Update the display with the current value
+function updateDisplay() {
+
+    display.textContent = firstOperand + '' + operator + '' + secondOperand;
+
+}
+
+
